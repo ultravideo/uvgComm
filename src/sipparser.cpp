25433d120a55e12772c7bf68e7b1ca325e3205c0
Joni Räsänen <joni.rasanen@tut.fi>
2017-02-27 15:17:43 +0200

    SIP message parser implementation with message being first sorted into a table and then converted to struct. Struct conversion missing.

--- src/sipparser.cpp
+++ src/sipparser.cpp
@@ -3,31 +3,154 @@
 #include <iostream>
 #include <sstream>
 
+#include <QList>
+
+struct HeaderLine
+{
+  QString name;
+  quint8 words;
+};
+
+// remember to update tableToInfo if adding new header fields!
+const QList<HeaderLine> HEADERLINES(
+{
+      {"Via:", 3},
+      {"Max-Forwards:", 2},
+      {"To:", 3},
+      {"From:", 3},
+      {"Call-ID:", 2},
+      {"Cseq:", 3},
+      {"Contact:", 2},
+      {"Content-Type:", 2},
+      {"Content-Length:", 2},
+}
+);
+
+// TODO this changes depending on the message type, maybe make a function/table for that
+const uint16_t MANDATORYLINES = 9;
+const uint16_t FIRSTLINEWORDS = 3;
 
 
+bool parseMethodName(SIPMessageInfo* info, QString line);
+
+void messageToTable(QStringList& lines, QList<QStringList> &values);
+void tableToInfo(QList<QStringList>& values, SIPMessageInfo* info);
+
+bool checkSIPMessage(QList<QStringList>& values);
+
+void cleanup(SIPMessageInfo* info)
+{
+  if(info != 0)
+    delete info;
+}
 
 
-void checkSIPLegality();
-
-
+// The basi logic goes as follow:
+// 1. Parse first line with method name
+// 2. then sort the header-fields to an array
+// 3. then set info fields from sorted array where everything is in place
 std::unique_ptr<SIPMessageInfo> parseSIPMessage(QString& header)
 {
   SIPMessageInfo* info = new SIPMessageInfo;
 
-  std::istringstream sip_string(header.toStdString());
-  QString line = "";
+  QStringList lines = header.split("\r\n");
 
-  /*
-  while(std::getline(sip_string, line))
+  qDebug() << "SIP message split into"  << lines.length() << "lines";
+
+  QList<QStringList> values;
+
+
+  if(!parseMethodName(info, lines.at(0)))
   {
-    std::istringstream sip_line(line.toStdString());
-    QString word = "";
+     cleanup(info);
+     return NULL;
+  }
 
-    while(sip_line >> word)
+  messageToTable(lines, values);
+  if(!checkSIPMessage(values))
+    return NULL;
+  tableToInfo(values, info);
+
+  return std::unique_ptr<SIPMessageInfo> (info);
+}
+
+bool parseMethodName(SIPMessageInfo* info, QString line)
+{
+  qDebug() << "Parsing method name";
+  QStringList words = line.split(" ");
+
+  if(words.length() != FIRSTLINEWORDS)
+  {
+    qDebug() << "First line had wrong number of words:"<< words.length()
+             << "Expected:" << FIRSTLINEWORDS;
+    qDebug() << "Tried to parse following line:" << line;
+    return false;
+  }
+
+  if(words.at(0) == "INVITE")
+  {
+    info->request = INVITE;
+    qDebug() << "INVITE found";
+  }
+  else
+  {
+    qDebug() << "Unrecognized Method:" << words.at(0);
+  }
+
+  return true;
+}
+
+void messageToTable(QStringList& lines, QList<QStringList> &values)
+{
+  qDebug() << "Sorting sip message to table for lookup";
+
+  for(unsigned int i = 1;  i < lines.length(); ++i)
+  {
+    qDebug() << "Line" << i + 1 << "contents:" << lines.at(i);
+    QStringList words = lines.at(i).split(" ");
+
+    for(auto field : HEADERLINES)
     {
-      QDebug() << word;
+      if(QString::compare(words.at(0), field.name, Qt::CaseInsensitive) == 0)
+      {
+        // increase the list size to
+        while(values.size() < i)
+        {
+          values.append(QStringList(""));
+        }
+
+        values[i - 1] = words;
+      }
     }
   }
-  */
-  return 0;
+}
+
+void tableToInfo(QList<QStringList>& values, SIPMessageInfo* info)
+{
+  qDebug() << "";
+}
+
+bool checkSIPMessage(QList<QStringList>& values)
+{
+  qDebug() << "Checking SIP message";
+
+  if(values.size() < MANDATORYLINES)
+  {
+    qDebug() << "Found SIP message with too few lines:" << values.size() << "lines";
+    return false;
+  }
+
+  for(unsigned int i = 0; i < MANDATORYLINES; ++i)
+  {
+    if(values[i].length() != HEADERLINES.at(i).words)
+    {
+      qDebug() << "Wrong number of words in a line for:" << HEADERLINES.at(i).name
+               << "(" << i << ")"
+               <<". Found:" << values[i].length()
+               << "Expecting:" << HEADERLINES.at(i).words;
+      qDebug() << "Line:" << values[i];
+      return false;
+    }
+
+  }
 }
--- src/sipparser.h
+++ src/sipparser.h
@@ -8,7 +8,7 @@
 #include "common.h"
 #include <memory>
 
-
+// TODO maybe combine this with other sip message structures such as session or compser message structure
 struct SIPMessageInfo
 {
   MessageType request;
@@ -27,7 +27,7 @@ struct SIPMessageInfo
   QString ourUsername;
   QString ourLocation;
   QString ourTag;
-  QString replyAddress;
+  QString replyAddress; // TODO more than one via possible
   QString branch;
 
   QString callID;
@@ -37,7 +37,6 @@ struct SIPMessageInfo
   QString contentType;
   QString content;
 };
-
 
   // returns a null pointer if parsing is not successful
   std::unique_ptr<SIPMessageInfo> parseSIPMessage(QString& header);
