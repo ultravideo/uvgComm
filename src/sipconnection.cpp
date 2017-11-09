e0c338036d8a47e4ae710b5a6023b1b9cd84cb53
Joni Räsänen <joni.rasanen@tut.fi>
2017-11-09 16:30:45 +0200

    First version of SIP Connection/Routing.

--- src/sipconnection.cpp
+++ src/sipconnection.cpp
@@ -1,45 +1,133 @@
 #include "sipconnection.h"
 
-SIPConnection::SIPConnection()
-{
+#include <QDebug>
 
-}
+SIPConnection::SIPConnection():
+  localUsername_(""),
+  localHost_(""),
+  localDirectAddress_(""),
+  remoteUsername_(""),
+  remoteHost_(""),
+  sessionHost_(""),
+  previousReceivedRequest_(NULL),
+  previousSentRequest_(NULL)
+{}
 
-
-void SIPConnection::initLocal(QString localUsername, QString localHost, QString sessionHost, QString localAddress)
+void SIPConnection::initLocal(QString localUsername, QString localHost, QString localAddress, QString sessionHost)
 {
   localUsername_ = localUsername;
   localHost_ = localHost;       // name of our sip server
-
   localDirectAddress_ = localAddress;
 }
 
-// return whether the incoming routing info is valid
+void SIPConnection::initRemote(QString remoteUsername, QString remoteHost)
+{
+  remoteUsername_ = remoteUsername;
+  remoteHost_ = remoteHost;
+}
+
 bool SIPConnection::incomingSIPRequest(std::shared_ptr<SIPRouting> routing)
 {
-  if(!checkMessageValidity(routing))
+  if(remoteUsername_ == "" && remoteHost_ == "")
   {
-    return false;
+    remoteUsername_ = routing->senderUsername;
+    remoteHost_ = routing->senderHost;
   }
 
+  if(routing->senderUsername != remoteUsername_ ||
+     routing->senderHost != remoteHost_ ||
+     routing->receiverUsername != localUsername_ ||
+     routing->receiverHost != localHost_)
+  {
+    qDebug() << "Incoming SIP Request sender:" << routing->senderUsername << "@" << routing->senderHost
+             << "->" << routing->receiverUsername << "@" << routing->receiverHost << "does not match local info. Them:"
+             << remoteUsername_ << "@" << remoteHost_ << "-> Us" << localUsername_ << "@" << localHost_;
+    return false;
+  }
+  previousReceivedRequest_ = routing;
   return true;
 }
 
-std::shared_ptr<SIPRouting> SIPConnection::requestRouting()
+bool SIPConnection::incomingSIPResponse(std::shared_ptr<SIPRouting> routing)
 {
+  if(previousSentRequest_ == NULL)
+  {
+    qDebug() << "OTHER ERROR: Got a response without sending any requests!!!";
+    return false;
+  }
+
+  // check if this response matches the request sent.
+  if(routing->senderUsername   != previousSentRequest_->senderUsername ||
+     routing->senderHost       != previousSentRequest_->senderHost ||
+     routing->receiverUsername != previousSentRequest_->receiverUsername ||
+     routing->receiverHost     != previousSentRequest_->receiverHost ||
+     routing->sessionHost != previousSentRequest_->sessionHost)
+  {
+    qDebug() << "OTHER ERROR: The incoming SIP response "
+                "does not have the same fields as the sent request";
+    return false;
+  }
+
+  if(routing->contactAddress == previousSentRequest_->contactAddress)
+  {
+    qDebug() << "OTHER ERROR: They have not set their contact field correctly";
+    return false;
+  }
+
+  qDebug() << "Request via:" << previousSentRequest_->senderReplyAddress.at(0)
+           << "Response # vias:" << routing->senderReplyAddress.size();
+
+  remoteDirectAddress_ = routing->contactAddress;
+}
+
+std::shared_ptr<SIPRouting> SIPConnection::requestRouting(QString &directAddress)
+{
+  if(localUsername_.isEmpty() ||
+     localHost_.isEmpty() ||
+     localDirectAddress_.isEmpty() ||
+     remoteUsername_.isEmpty() ||
+     remoteHost_.isEmpty() ||
+     sessionHost_.isEmpty())
+  {
+    qDebug() << "WARNING: Trying to get request routing without initialization!!";
+    return NULL;
+  }
+
   std::shared_ptr<SIPRouting> newRouting(new SIPRouting);
 
   newRouting->senderUsername = localUsername_;
   newRouting->senderHost = localHost_;
   newRouting->senderReplyAddress.append(localDirectAddress_);
-  newRouting->senderDirectAddress = localDirectAddress_;
+  newRouting->contactAddress = localDirectAddress_;
 
+  newRouting->receiverHost = remoteUsername_;
   newRouting->receiverHost = remoteHost_;
+
+  newRouting->sessionHost = sessionHost_;
+
+  if(!remoteDirectAddress_.isEmpty())
+  {
+    directAddress = remoteDirectAddress_;
+  }
 
   return newRouting;
 }
 
-bool SIPConnection::checkMessageValidity(std::shared_ptr<SIPRouting> routing)
+// copies the fields from previous request
+std::shared_ptr<SIPRouting> SIPConnection::responseRouting()
 {
+  Q_ASSERT(previousReceivedRequest_);
+  if(!previousReceivedRequest_)
+  {
+    qDebug() << "WARNING: Trying to send a response when "
+                "there is no previous request to copy for response routing";
+    return NULL;
+  }
 
+  std::shared_ptr<SIPRouting> newRouting(new SIPRouting);
+  //everything expect contact field are copied from received request
+  *newRouting = *previousReceivedRequest_;
+  newRouting->contactAddress = localDirectAddress_;
+
+  return newRouting;
 }
--- src/sipconnection.h
+++ src/sipconnection.h
@@ -12,7 +12,7 @@ struct SIPRouting
   QString senderUsername;
   QString senderHost;
   QList<QString> senderReplyAddress;   // from via-fields. Send responses here by copying these.
-  QString senderDirectAddress;  // from contact field. Send requests here
+  QString contactAddress;  // from contact field. Send requests here to bypass server
 
   QString receiverUsername;
   QString receiverHost;
@@ -28,33 +28,37 @@ class SIPConnection
 public:
   SIPConnection();
 
-  // if localHost is empty, assume this is a peer to peer session
-  void initLocal(QString localUsername, QString localHost, QString sessionHost, QString localAddress);
+  // for peer-to-peer calls, use ip addresses as both host
+  void initLocal(QString localUsername, QString sessionHost, QString localAddress, QString localHost);
 
+  // use this if sending the first request. Not needed if the first request is sent by remote.
   void initRemote(QString remoteUsername, QString remoteHost);
 
+  // returns whether the incoming info is valid.
+  // Remember to process request in order of arrival
+  // Does not check if we are allowed to receive requests from this user.
   bool incomingSIPRequest(std::shared_ptr<SIPRouting> routing);
   bool incomingSIPResponse(std::shared_ptr<SIPRouting> routing);
 
-  std::shared_ptr<SIPRouting> requestRouting();
+  // returns values to be used in SIP request. Sets directaddress if we have it.
+  std::shared_ptr<SIPRouting> requestRouting(QString& directAddress);
 
   // copies the fields from previous request
+  // sets direct contact as per RFC.
   std::shared_ptr<SIPRouting> responseRouting();
 
 private:
 
-  bool checkMessageValidity(std::shared_ptr<SIPRouting> routing);
-
-  QString localUsername_;
+  QString localUsername_;      //our username on sip server
   QString localHost_;          // name of our sip server
   QString localDirectAddress_; // to contact and via fields
 
   QString remoteUsername_;
-  QString remoteHost_;          // name of their sip server
-  QString remoteDirectAddress_; // from contact field
-
-  QList<QString> remoteReplyAddress_;   // from via-field
-  QString remoteContactAddress_;
+  QString remoteHost_;          // name of their sip server or their ip address
+  QString remoteDirectAddress_; // from contact field. Send requests here if not empty.
 
   QString sessionHost_;
+
+  std::shared_ptr<SIPRouting> previousReceivedRequest_;
+  std::shared_ptr<SIPRouting> previousSentRequest_;
 };
