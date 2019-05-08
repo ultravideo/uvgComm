b8824222bee3b5e9924ef156096c28a497488b9b
Joni Räsänen <joni.rasanen@tut.fi>
2019-05-08 12:37:42 +0300

    refactor(global): Uniform printing of debug information.
    Added functions for printing debug information so that all debug prints will be
    formulated in the same way.

--- src/common.cpp
+++ src/common.cpp
@@ -7,6 +7,8 @@
 #include <windows.h> // for Sleep
 #endif
 
+#include <QDebug>
+
 // TODO move this to a different file from common.h
 void qSleep(int ms)
 {
@@ -35,4 +37,70 @@ QString generateRandomString(uint32_t length)
     string.append(alphabet.at(qrand()%alphabet.size()));
   }
   return string;
+}
+
+
+void printDebugObject(DebugType type, QObject* object,
+                      QString context, QString description,
+                      QStringList valueNames, QStringList values)
+{
+  printDebug(type, object->metaObject()->className(), context, description, valueNames, values);
+}
+
+void printDebug(DebugType type, QString className, QString context, QString description, QStringList valueNames, QStringList values)
+{
+
+  QString valueString = "";
+  if (valueNames.size() == values.size())
+  {
+    for (int i = 0; i < valueNames.size(); ++i)
+    {
+      valueString.append(valueNames.at(i));
+      valueString.append(": ");
+      valueString.append(values.at(i));
+      if (i != valueNames.size() - 1)
+      {
+        valueString.append("\r\n");
+      }
+    }
+  }
+  else {
+    qDebug() << "Value printing failed";
+  }
+
+  // This could be reduced, but it might change so not worth probably at the moment.
+  // Choose which text to print based on type.
+  switch (type) {
+  case DEBUG_NORMAL:
+  {
+    qDebug().nospace().noquote() << context << ", " << className << ": "
+                                 << description << " " << valueString;
+    break;
+  }
+  case DEBUG_ERROR:
+  {
+    qCritical().nospace().noquote() << "ERROR: --------------------------------------------";
+    qCritical().nospace().noquote() << context << ", " << className << ": "
+                                 << description << " " << valueString;
+    qCritical().nospace().noquote() << "-------------------------------------------- ERROR";
+    break;
+  }
+  case DEBUG_WARNING:
+  {
+    qWarning().nospace().noquote() << "WARNING: --------------------------------------------";
+    qWarning().nospace().noquote() << context << ", " << className << ": "
+                                 << description << " " << valueString;
+    qWarning().nospace().noquote() << "-------------------------------------------- WARNING";
+    break;
+  }
+  case DEBUG_PEER_ERROR:
+  {
+    qWarning().nospace().noquote() << "PEER ERROR: --------------------------------------------";
+    qWarning().nospace().noquote() << context << ", " << className << ": "
+                                 << description << " " << valueString;
+    qWarning().nospace().noquote() << "-------------------------------------------- PEER ERROR";
+    break;
+  }
+
+  }
 }
--- src/common.h
+++ src/common.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <QList>
 #include <QString>
+#include <QObject>
 #include <stdint.h>
 
 // A module for functions used multiple times across the whole program
@@ -9,3 +10,16 @@
 void qSleep(int ms);
 
 QString generateRandomString(uint32_t length);
+
+enum DebugType{DEBUG_NORMAL, DEBUG_ERROR, DEBUG_WARNING, DEBUG_PEER_ERROR};
+
+
+// use this if printing happens inside class derived from QObject which is most Qt-classes
+void printDebugObject(DebugType type, QObject* object,
+                      QString context = "", QString description = "",
+                      QStringList valueNames = {}, QStringList values = {});
+
+// print debug information
+void printDebug(DebugType type, QString className,
+                QString context = "", QString description = "",
+                QStringList valueNames = {}, QStringList values = {});
