692cecc7952c0345360432f0f03506d58a3ed870
Aaro Altonen <aaro.altonen@tuni.fi>
2019-05-17 09:04:29 +0300

    feature(Delivery): Rewrite RTPSinkFilter to support the new RTP Library

--- src/media/delivery/rtpsinkfilter.cpp
+++ src/media/delivery/rtpsinkfilter.cpp
@@ -1,86 +1,133 @@
-#include "rtpsinkfilter.h"
+#include <QDebug>
+#include <cstdio>
 
-#include <statisticsinterface.h>
+#include "statisticsinterface.h"
+#include "rtpsinkfilter.h"
 #include "common.h"
 
-#include <RTPInterface.hh>
+const uint32_t BUFFER_SIZE = 10 * 65536;
 
-#include <QDebug>
+#define RTP_HEADER_SIZE 2
+#define FU_HEADER_SIZE  1
 
-const uint32_t BUFFER_SIZE = 10*65536;
-
-RTPSinkFilter::RTPSinkFilter(QString id, StatisticsInterface *stats, UsageEnvironment& env, DataType type, QString media):
-  Filter(id, "RTP_Sink_" + media, stats, NONE, type),
-  MediaSink(env),
-  type_(type),
-  addStartCodes_(false)
+static void __receiveHook(void *arg, RTPGeneric::GenericFrame *frame)
 {
-  fReceiveBuffer = new u_int8_t[BUFFER_SIZE];
+  if (arg && frame)
+  {
+    static_cast<RTPSinkFilter *>(arg)->receiveHook(frame);
+  }
+}
+
+RTPSinkFilter::RTPSinkFilter(QString id, StatisticsInterface *stats, DataType type, QString media, RTPReader *reader):
+  Filter(id, "RTP_Sink_" + media, stats, NONE, type),
+  type_(type),
+  addStartCodes_(true),
+  reader_(reader)
+{
+  qDebug() << "\n\n\nCREATING RTPSinkFilter!!\n\n\n";
   getStats()->addFilter(getName(), (uint64_t)currentThreadId());
+
+  reader_->installReceiveHook(this, __receiveHook);
 }
 
 RTPSinkFilter::~RTPSinkFilter()
-{}
+{
+}
 
 void RTPSinkFilter::uninit()
 {
-  stopPlaying();
-  while (fSource  != nullptr)
-  {
-    qSleep(1);
-  }
-  qDebug() << "Deleting RTPSink:" << getName() << "type:" << type_;
-  delete fReceiveBuffer;
-  fReceiveBuffer = nullptr;
 }
 
-void RTPSinkFilter::afterGettingFrame(void* clientData,
-                              unsigned frameSize,
-                              unsigned numTruncatedBytes,
-                              struct timeval presentationTime,
-                              unsigned durationInMicroseconds)
+void RTPSinkFilter::process()
 {
-  Q_ASSERT(clientData);
-  RTPSinkFilter* sink = (RTPSinkFilter*)clientData;
-  sink->afterGettingFrame(frameSize, numTruncatedBytes,
-                          presentationTime, durationInMicroseconds);
 }
 
-void RTPSinkFilter::afterGettingFrame(unsigned frameSize,
-                       unsigned numTruncatedBytes,
-                       struct timeval presentationTime,
-                       unsigned durationInMicroseconds)
+void RTPSinkFilter::start()
 {
+}
 
-  Q_UNUSED(durationInMicroseconds);
+void RTPSinkFilter::receiveHook(RTPGeneric::GenericFrame *frame)
+{
+  RTPGeneric::GenericFrame *f = frame;
 
-  // TODO increase buffer for large frames
-  Q_ASSERT(numTruncatedBytes == 0);
+  getStats()->addReceivePacket(frame->dataLen);
 
-  getStats()->addReceivePacket(frameSize);
-
-  if(addStartCodes_ && type_ == HEVCVIDEO)
+  if (addStartCodes_ && type_ == HEVCVIDEO)
   {
-    //qDebug() << "Received HEVC frame. Size: " << frameSize
-    //         << ", truncated: " << numTruncatedBytes;
-    frameSize += 4;
+    // fragmentation unit
+    if ((frame->data[0] >> 1) == 49)
+    {
+      bool firstFrag = frame->data[2] & 0x80;
+      bool lastFrag  = frame->data[2] & 0x40;
+
+      if (firstFrag && lastFrag)
+      {
+        qDebug() << "PEER ERROR: invalid combination of S and E bits!";
+        return;
+      }
+
+      // TODO should this be part of rtp lib instead?
+
+      if (firstFrag)
+      {
+        // create new NAL header for fragmented frame
+        uint8_t NALHeader[2] = { 0 };
+        NALHeader[0] = (frame->data[0] & 0x81) | ((frame->data[2] & 0x3f) << 1);
+        NALHeader[1] = frame->data[1];
+
+        // discard RTP and FU headers
+        frame->data    += (RTP_HEADER_SIZE + FU_HEADER_SIZE);
+        frame->dataLen -= (RTP_HEADER_SIZE + FU_HEADER_SIZE);
+
+        memcpy(fragFrame_,                     NALHeader,   sizeof(NALHeader));
+        memcpy(&fragFrame_[sizeof(NALHeader)], frame->data, frame->dataLen);
+
+        fragSize_ = sizeof(NALHeader) + frame->dataLen;
+
+        // start of the fragmented frame, no reason to continue processing just yet
+        // so return early. The frame is processed when the last fragmentation unit is received
+        return;
+      }
+      else
+      {
+        // discard RTP and FU headers
+        frame->data    += (RTP_HEADER_SIZE + FU_HEADER_SIZE);
+        frame->dataLen -= (RTP_HEADER_SIZE + FU_HEADER_SIZE);
+
+        // append this fragmentation unit at the end of frag buffer
+        memcpy(&fragFrame_[fragSize_], frame->data, frame->dataLen);
+        fragSize_ += frame->dataLen;
+
+        // we haven't received the last fragmentation unit so exit early
+        if (!lastFrag)
+        {
+          return;
+        }
+
+        // we've received the full frame
+        f->data    = fragFrame_;
+        f->dataLen = fragSize_;
+        fragSize_  = 0;
+      }
+    }
+
+    f->dataLen += 4;
   }
 
   Data *received_picture = new Data;
-  received_picture->data_size = frameSize;
+  received_picture->data_size = f->dataLen;
   received_picture->type = type_;
   received_picture->data = std::unique_ptr<uchar[]>(new uchar[received_picture->data_size]);
   received_picture->width = 0; // not know at this point. Decoder tells the correct resolution
   received_picture->height = 0;
   received_picture->framerate = 0;
-  received_picture->presentationTime = presentationTime;
   received_picture->source = REMOTE;
 
   // TODO: This copying should be done in separate thread as in
   // framedsource if we want to receive 4K with less powerful thread (like in Xeon)
-  if(addStartCodes_ && type_ == HEVCVIDEO)
+  if (addStartCodes_ && type_ == HEVCVIDEO)
   {
-    memcpy(received_picture->data.get() + 4, fReceiveBuffer, received_picture->data_size - 4);
+    memcpy(received_picture->data.get() + 4, f->data, received_picture->data_size - 4);
     received_picture->data[0] = 0;
     received_picture->data[1] = 0;
     received_picture->data[2] = 0;
@@ -88,24 +135,9 @@ void RTPSinkFilter::afterGettingFrame(unsigned frameSize,
   }
   else
   {
-    memcpy(received_picture->data.get(), fReceiveBuffer, received_picture->data_size);
+    memcpy(received_picture->data.get(), f->data, received_picture->data_size);
   }
 
   std::unique_ptr<Data> rp( received_picture );
   sendOutput(std::move(rp));
-
-  continuePlaying();
-}
-
-void RTPSinkFilter::process()
-{}
-
-Boolean RTPSinkFilter::continuePlaying() {
-  if (fSource == nullptr) return False; // sanity check (should not happen)
-
-  // Request the next frame of data from our input source.  "afterGettingFrame()" will get called later, when it arrives:
-  fSource->getNextFrame(fReceiveBuffer, BUFFER_SIZE,
-                        afterGettingFrame, this,
-                        onSourceClosure, this);
-  return True;
 }
--- src/media/delivery/rtpsinkfilter.h
+++ src/media/delivery/rtpsinkfilter.h
@@ -1,9 +1,40 @@
 #pragma once
 #include "media/processing/filter.h"
-
-#include <H265VideoRTPSink.hh>
+#include "filter.h"
+#include "../rtplib/src/reader.hh"
 
 // Receives RTP packets and sends them forward in filter graph.
+
+class RTPSinkFilter : public Filter
+{
+public:
+  RTPSinkFilter(QString id, StatisticsInterface *stats, DataType type, QString media, RTPReader *reader);
+  ~RTPSinkFilter();
+
+  void receiveHook(RTPGeneric::GenericFrame *frame);
+
+  void uninit();
+  void start();
+
+protected:
+  void process();
+
+private:
+  // TODO why this must be static
+
+  DataType type_;
+  bool addStartCodes_;
+
+  RTPReader *reader_;
+
+  uint8_t fragFrame_[16384];
+
+  std::vector<RTPGeneric::GenericFrame *> fragBuf_;
+  size_t fragSize_;
+};
+
+#if 0
+/* #include <H265VideoRTPSink.hh> */
 
 class RTPSinkFilter : public Filter, public MediaSink
 {
@@ -37,3 +68,4 @@ protected:
 
   bool addStartCodes_;
 };
+#endif
