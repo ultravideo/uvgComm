c7b9e854fbd3008f3120a69f385781e2598b33fd
Aaro Altonen <aaro.altonen@tuni.fi>
2019-06-10 12:47:22 +0300

    feature(Delivery): Create Interface for RTP Streamers

---
+++ src/media/delivery/irtpstreamer.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "media/processing/filter.h"
+
+#include <QHostAddress>
+
+class IRTPStreamer
+{
+public:
+  virtual ~IRTPStreamer();
+
+  virtual void init(StatisticsInterface *stats) = 0;
+  virtual void uninit() = 0;
+  virtual void run() = 0;
+  virtual void stop() = 0;
+
+  // init a session with sessionID to use with add/remove functions
+  // returns whether operation was successful
+  virtual bool addPeer(QHostAddress address, uint32_t sessionID) = 0;
+
+  // Returns filter to be attached to filter graph. ownership is not transferred.
+  // removing the peer or stopping the streamer destroys these filters.
+  virtual std::shared_ptr<Filter> addSendStream(uint32_t peer, uint16_t port, QString codec, uint8_t rtpNum) = 0;
+  virtual std::shared_ptr<Filter> addReceiveStream(uint32_t peer, uint16_t port, QString codec, uint8_t rtpNum) = 0;
+
+  virtual void removeSendVideo(uint32_t sessionID) = 0;
+  virtual void removeSendAudio(uint32_t sessionID) = 0;
+  virtual void removeReceiveVideo(uint32_t sessionID) = 0;
+  virtual void removeReceiveAudio(uint32_t sessionID) = 0;
+
+  // removes everything related to this peer
+  virtual void removePeer(uint32_t sessionID) = 0;
+
+  virtual void removeAllPeers() = 0;
+};
