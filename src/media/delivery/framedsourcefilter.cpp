7a459fb5e47812b3efb494e34599f7857979d199
Aaro Altonen <aaro.altonen@tuni.fi>
2019-06-10 08:54:43 +0300

    refactor(Delivery): Remove unused code from FramedSourceFilter

--- src/media/delivery/framedsourcefilter.cpp
+++ src/media/delivery/framedsourcefilter.cpp
@@ -12,7 +12,6 @@ FramedSourceFilter::FramedSourceFilter(QString id, StatisticsInterface *stats,
   Filter(id, "Framed_Source_" + media, stats, type, NONE),
   type_(type),
   stop_(false),
-  separateInput_(false), // TODO mikä järki tässä on???
   writer_(writer),
   frame_(0)
 {
@@ -23,9 +22,11 @@ FramedSourceFilter::FramedSourceFilter(QString id, StatisticsInterface *stats,
     case HEVCVIDEO:
       dataFormat_ = RTP_FORMAT_HEVC;
       break;
+
     case OPUSAUDIO:
       dataFormat_ = RTP_FORMAT_OPUS;
       break;
+
     default:
       dataFormat_ = RTP_FORMAT_GENERIC;
       break;
@@ -68,99 +69,41 @@ void FramedSourceFilter::updateSettings()
     }
 
      qDebug() << "Settings," << getName() << ": updated buffersize to" << maxBufferSize_;
-
-     if (settings.value("video/liveCopying").isValid())
-     {
-       separateInput_ = (settings.value("video/liveCopying").toInt() == 1);
-     }
-     else
-     {
-       qDebug() << "ERROR: Missing settings value flip threads";
-     }
   }
 }
 
 void FramedSourceFilter::start()
 {
-  qDebug() << "\n\n\nDUMMY FRAMEDSOURCEFILTER::START\n\n\n";
-
-  if (separateInput_)
-  {
-    /* TODO:  */
-    /* afterEvent_ = envir().taskScheduler().createEventTrigger((TaskFunc*)FramedSource::afterGetting); */
-
-    qDebug() << "Iniating, FramedSource : Creating trigger for framedSource:";
-             //<< getName() << "Trigger ID: " << afterEvent_;
-  }
-
   Filter::start();
 }
 
 void FramedSourceFilter::stop()
 {
-  qDebug() << "\n\n\nDUMMY FRAMEDSOURCEFILTER::STOP\n\n\n";
-  /* TODO:  */
+  Filter::stop();
+  stop_ = true;
 }
 
 void FramedSourceFilter::process()
 {
-  /* qDebug() << "\n\n\nDUMMY FRAMEDSOURCEFILTER::PROCESS\n\n\n"; */
+  rtp_error_t ret;
+  std::unique_ptr<Data> currentFrame;
 
-  while (true)
+  while (!stop_)
   {
-    /* framePointerReady_.acquire(1); */
-
-    if (stop_)
+    while ((currentFrame = getInput()) != nullptr)
     {
-      return;
-    }
+      ret = writer_->push_frame(currentFrame->data.get(),
+                                currentFrame->data_size,
+                                dataFormat_, (90000 / 24) * ++frame_);
 
-    std::unique_ptr<Data> currentFrame = getInput();
-
-    // TODO mitä ihmettä tässä tapahtuu?
-    if (currentFrame == nullptr)
-    {
-      /* fFrameSize = 0; */
-      /* sendFrame(); */
-      qDebug() << "hmmmm...";
-      return;
-    }
-
-    while (currentFrame)
-    {
-      /* qDebug() << "do stuff"; */
-      /* copyFrameToBuffer(std::move(currentFrame)); */
-      /* sendFrame(); */
-
-      frame_++;
-      if (writer_->push_frame(currentFrame->data.get(), currentFrame->data_size, dataFormat_, (90000 / 24) * frame_) < 0)
+      if (ret != RTP_OK)
       {
+        printDebug(DEBUG_ERROR, this, DC_PROCESS_MEDIA, "Failed to send data", { "Error" }, { QString(ret) });
         qDebug() << "failed to send data!";
         break;
       }
 
       getStats()->addSendPacket(currentFrame->data_size);
-      currentFrame = nullptr;
-
-      // TODO mitä ihmeen säätöä tämä loppu on?
-
-      /* framePointerReady_.acquire(1); */
-      if (stop_)
-      {
-        return;
-      }
-
-      // TODO mitä ihmettä tässä tapahtuu?
-      currentFrame = getInput();
-
-      // copy additional NAL units, if available.
-      if(currentFrame == nullptr)
-      {
-        /* fFrameSize = 0; */
-        /* sendFrame(); */
-        return;
-      }
     }
   }
-
 }
--- src/media/delivery/framedsourcefilter.h
+++ src/media/delivery/framedsourcefilter.h
@@ -19,75 +19,15 @@ public:
   void start();
   void stop();
 
-  void copyFrameToBuffer(std::unique_ptr<Data> currentFrame);
-
 protected:
   void process();
 
 private:
-  DataType type_; // TODO use output_
+  DataType type_;
   bool stop_;
-  bool separateInput_;
   bool removeStartCodes_;
 
   kvz_rtp::writer *writer_;
   uint64_t frame_;
   rtp_format_t dataFormat_;
 };
-
-#if 0
-/* #ifdef _WIN32 */
-class FramedSourceFilter : public Filter
-{
-public:
-  FramedSourceFilter(QString id, StatisticsInterface *stats,
-                     DataType type, QString media, QMutex *triggerMutex,
-                     bool live555Copying);
-
-  ~FramedSourceFilter();
-
-  // called by live555. Takes a sample from input and schedules it to be sent.
-  virtual void doGetNextFrame();
-
-  virtual void updateSettings();
-
-  virtual Boolean isH265VideoStreamFramer() const
-  {
-    return type_ == HEVCVIDEO;
-  }
-
-  virtual void start();
-  virtual void stop();
-protected:
-  void process();
-
-  // FramedSource function.
-  virtual void doStopGettingFrames();
-
-private:
-
-  // copy the frame to FramedSource buffer
-  void copyFrameToBuffer(std::unique_ptr<Data> currentFrame);
-
-  // send frame over the network.
-  void sendFrame();
-
-
-  DataType type_; // TODO use output_
-
-  QSemaphore framePointerReady_;
-
-  EventTriggerId afterEvent_;
-
-  QMutex* triggerMutex_;
-
-  bool separateInput_;
-  bool ending_;
-  bool removeStartCodes_;
-
-  TaskToken currentTask_;
-
-  bool stop_;
-  bool noMoreTasks_;
-};
-#endif
