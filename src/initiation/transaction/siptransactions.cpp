bccc4163b333c08765a49c4e3bb50b2ba5813e17
Joni Räsänen <joni.rasanen@tut.fi>
2020-01-23 15:14:14 +0200

    refactor(Initiation): Set values for SIP URI initializations.

--- src/initiation/transaction/siptransactions.cpp
+++ src/initiation/transaction/siptransactions.cpp
@@ -60,7 +60,7 @@ void SIPTransactions::startCall(Contact &address, QHostAddress localAddress,
   std::shared_ptr<SIPDialogData> dialogData;
   createBaseDialog(sessionID, dialogData);
   dialogData->state->createNewDialog(SIP_URI{TRANSPORTTYPE, address.username, address.username,
-                                             address.remoteAddress,  0},
+                                             address.remoteAddress,  0, {}},
                                      localAddress.toString(), registered);
 
   // this start call will commence once the connection has been established
