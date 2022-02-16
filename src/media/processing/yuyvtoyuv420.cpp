01af8e8d2a56243de944c3227e306729e2a44086
Joni Räsänen <joni.rasanen@iki.fi>
2022-02-16 14:34:12 +0200

    feature(Processing): Convert luma pixels from YUYV to YUV420
    Still working on chroma

--- src/media/processing/yuyvtoyuv420.cpp
+++ src/media/processing/yuyvtoyuv420.cpp
@@ -12,7 +12,7 @@ void YUYVtoYUV420::process()
 
   while(input)
   {
-    uint32_t finalDataSize = input->width*input->height*2;
+    uint32_t finalDataSize = input->width*input->height + input->width*input->height/2;
     std::unique_ptr<uchar[]> yuyv_frame(new uchar[finalDataSize]);
 
     yuyv2yuv420_cpu(input->data.get(), yuyv_frame.get(), input->width, input->height);
@@ -30,5 +30,10 @@ void YUYVtoYUV420::process()
 void YUYVtoYUV420::yuyv2yuv420_cpu(uint8_t* input, uint8_t* output,
                                    uint16_t width, uint16_t height)
 {
-
+  // Luma pixels
+  for(int i = 0; i < width*height; i += 2)
+  {
+    output[i] = input[i*2];
+    output[i + 1] = input[i*2 + 2];
+  }
 }
