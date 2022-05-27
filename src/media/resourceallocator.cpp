77322a5eb7ba400bfa1a90c2d2d95225b9d4e78e
Joni Räsänen <joni.rasanen@iki.fi>
2022-05-27 17:30:45 +0300

    feature(Media): Start estimating the bit rate based on RTCP reports
    Not the full feature, but some of the needed structures. Missing
    finding the lowest suitable bit rate.

--- src/media/resourceallocator.cpp
+++ src/media/resourceallocator.cpp
@@ -6,12 +6,22 @@
 #include "logger.h"
 #include "common.h"
 
-ResourceAllocator::ResourceAllocator()
-{
-  sse41_ = is_sse41_available();
-  avx2_  = is_avx2_available();
-  manualROI_ = false;
-}
+const int MIN_OPUS_BITRATE_BITS = 16000;
+const int MAX_OPUS_BITRATE_BITS = 24000;
+const int MIN_HEVC_BITRATE_BITS = 150000;
+const int MAX_HEVC_BITRATE_BITS = 1500000;
+
+
+ResourceAllocator::ResourceAllocator():
+  avx2_(is_avx2_available()),
+  sse41_(is_sse41_available()),
+  manualROI_(false),
+  audioStreams_(),
+  videoStreams_(),
+  bitrateMutex_(),
+  videoBitrate_(MAX_HEVC_BITRATE_BITS),
+  audioBitrate_(MAX_OPUS_BITRATE_BITS)
+{}
 
 
 void ResourceAllocator::updateSettings()
@@ -36,4 +46,118 @@ bool ResourceAllocator::isSSE41Enabled()
 bool ResourceAllocator::useManualROI()
 {
   return manualROI_;
+}
+
+
+void ResourceAllocator::addRTCPReport(uint32_t sessionID, DataType type,
+                                      int32_t lost, uint32_t jitter)
+{
+  std::shared_ptr<StreamInfo> info = getStreamInfo(sessionID, type);
+
+  if (jitter > info->previousJitter || lost > info->previousLost)
+  {
+    if (lost > info->previousLost)
+    {
+      info->bitrate /= 2;
+    }
+    else
+    {
+      info->bitrate *= 0.9;
+    }
+  }
+  else
+  {
+    info->bitrate *= 1.1;
+  }
+
+
+  limitBitrate(info->bitrate, type);
+
+  info->previousJitter = jitter;
+  info->previousLost = lost;
+
+  updateGlobalBitrate(info->bitrate, type);
+}
+
+
+void ResourceAllocator::updateGlobalBitrate(int bitrate, DataType type)
+{
+
+}
+
+
+int ResourceAllocator::getBitrate(DataType type)
+{
+  int bitrate = 0;
+
+  bitrateMutex_.lock();
+  if (type == DT_OPUSAUDIO)
+  {
+    bitrate = audioBitrate_;
+  }
+  else if (type == DT_HEVCVIDEO)
+  {
+    bitrate = videoBitrate_;
+  }
+  bitrateMutex_.unlock();
+
+  return bitrate;
+}
+
+
+std::shared_ptr<StreamInfo> ResourceAllocator::getStreamInfo(uint32_t sessionID, DataType type)
+{
+  std::shared_ptr<StreamInfo> pointer = nullptr;
+  if (type == DT_OPUSAUDIO)
+  {
+    if (audioStreams_.find(sessionID) == audioStreams_.end())
+    {
+      audioStreams_[sessionID] = std::shared_ptr<StreamInfo>(new StreamInfo{0, 0,
+                                                                            MAX_OPUS_BITRATE_BITS});
+    }
+
+    pointer = audioStreams_[sessionID];
+  }
+  else if (type == DT_HEVCVIDEO)
+  {
+    if (videoStreams_.find(sessionID) == videoStreams_.end())
+    {
+      videoStreams_[sessionID] = std::shared_ptr<StreamInfo>(new StreamInfo{0, 0,
+                                                                            MAX_HEVC_BITRATE_BITS});
+    }
+
+    pointer = videoStreams_[sessionID];
+  }
+  else
+  {
+    Logger::getLogger()->printUnimplemented(this, "Resource allocator tries to adjust unimplemented bit rate");
+  }
+  return pointer;
+}
+
+
+void ResourceAllocator::limitBitrate(int& bitrate, DataType type)
+{
+  if (type == DT_OPUSAUDIO)
+  {
+    if (bitrate < MIN_OPUS_BITRATE_BITS)
+    {
+      bitrate = MIN_OPUS_BITRATE_BITS;
+    }
+    else if (bitrate > MAX_OPUS_BITRATE_BITS)
+    {
+      bitrate = MAX_OPUS_BITRATE_BITS;
+    }
+  }
+  else if (type == DT_HEVCVIDEO)
+  {
+    if (bitrate < MIN_HEVC_BITRATE_BITS)
+    {
+      bitrate = MIN_HEVC_BITRATE_BITS;
+    }
+    else if (bitrate > MAX_HEVC_BITRATE_BITS)
+    {
+      bitrate = MAX_HEVC_BITRATE_BITS;
+    }
+  }
 }
--- src/media/resourceallocator.h
+++ src/media/resourceallocator.h
@@ -1,9 +1,19 @@
 #pragma once
 
+#include "src/media/processing/filter.h"
+
 #include <QObject>
 
 /* The purpose of this class is the enable filters to easily query the
  * state of hardware in terms of possible optimizations and performance. */
+
+struct StreamInfo
+{
+  uint32_t previousJitter;
+  int32_t  previousLost;
+
+  int bitrate;
+};
 
 class ResourceAllocator : public QObject
 {
@@ -18,10 +28,29 @@ public:
 
   bool useManualROI();
 
+  void addRTCPReport(uint32_t sessionID, DataType type,
+                     int32_t lost, uint32_t jitter);
+
+  int getBitrate(DataType type);
+
 private:
+
+  void updateGlobalBitrate(int bitrate, DataType type);
+
+  std::shared_ptr<StreamInfo> getStreamInfo(uint32_t sessionID, DataType type);
+
+  void limitBitrate(int &bitrate, DataType type);
 
   bool avx2_ = false;
   bool sse41_ = false;
 
   bool manualROI_ = false;
+
+  // key is sessionID
+  std::map<uint32_t, std::shared_ptr<StreamInfo>> audioStreams_;
+  std::map<uint32_t, std::shared_ptr<StreamInfo>> videoStreams_;
+
+  QMutex bitrateMutex_;
+  int videoBitrate_;
+  int audioBitrate_;
 };
