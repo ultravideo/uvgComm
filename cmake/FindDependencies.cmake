54ab6c8fa64c1a97e4b7b1581438c3c6287f1e7a
Joni Räsänen <joni.rasanen@iki.fi>
2023-04-13 08:34:51 +0300

    build(CMake): Build uvgRTP using fetchContent

--- cmake/FindDependencies.cmake
+++ cmake/FindDependencies.cmake
@@ -1,25 +1,15 @@
-find_package( Threads REQUIRED )
+include(FetchContent)
 
-#
 # Git
-#
 find_package(Git)
 
-#
-# GTest / GMock
-#
-include(FetchContent)
-FetchContent_Declare(
-        googletest
-        GIT_REPOSITORY https://github.com/google/googletest.git
-        GIT_TAG        release-1.11.0
-)
-# For Windows: Prevent overriding the parent project's compiler/linker settings
-set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
 
-FetchContent_MakeAvailable(googletest)
-add_library(GTest::GMock ALIAS gmock)
-add_library(GTest::GMockMain ALIAS gmock_main)
-add_library(GTest::GTest ALIAS gtest)
-add_library(GTest::GTestMain ALIAS gtest_main)
+message(STATUS "Fetching and building uvgRTP")
+# uvgRTP
+FetchContent_Declare(
+        uvgRTP
+        GIT_REPOSITORY https://github.com/ultravideo/uvgRTP.git
+        GIT_TAG        4423d6942627fd8458e488333b1f059ebe2d243c
+)
+
+FetchContent_MakeAvailable(uvgRTP)
