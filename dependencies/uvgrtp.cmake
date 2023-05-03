d1c796f220addb8817689fcaa5afa0bcac06455d
Joni Räsänen <joni.rasanen@iki.fi>
2023-05-03 09:31:21 +0300

    build(CMake): Always build Crypto++ for uvgRTP and unneeded parts of uvgRTP
    I added CMake flags to uvgRTP to:
    1) download Crypto++ headers automatically,
    2) Disable building of tests
    3) Disable building of examples
    4) Disable install
    
    1) enables Crypto++ to work inside Kvazzup with source build of uvgrtp.
    3) removes unnecessary targets from Qt Creator
    4) Removes functionality to install uvgRTP together with Kvazzup, since the
    install in uvgRTP does not work correctly.

--- dependencies/uvgrtp.cmake
+++ dependencies/uvgrtp.cmake
@@ -9,10 +9,15 @@ if (NOT UVGRTP_FOUND)
     FetchContent_Declare(
             uvgrtp
             GIT_REPOSITORY https://github.com/ultravideo/uvgRTP.git
-            GIT_TAG        4423d6942627fd8458e488333b1f059ebe2d243c
+            GIT_TAG        ce252c7f3442b045f6e04451f920bcd26d083ecb
     )
 
     set(BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
+
+    set(UVGRTP_DISABLE_TESTS ON CACHE BOOL "" FORCE)
+    set(UVGRTP_DISABLE_EXAMPLES  ON CACHE BOOL "" FORCE)
+    set(UVGRTP_DISABLE_INSTALL ON CACHE BOOL "" FORCE)
+    set(UVGRTP_DOWNLOAD_CRYPTO ON CACHE BOOL "" FORCE)
 
     FetchContent_MakeAvailable(uvgrtp)
 
