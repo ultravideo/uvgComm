2b6a6c46a973b4e16cdaee6e59888c2db580a02a
Heikki Tampio <heikki.tampio@tuni.fi>
2023-06-08 13:12:21 +0300

    fix(Processing): Use older version of uvgRTP

--- dependencies/uvgrtp.cmake
+++ dependencies/uvgrtp.cmake
@@ -8,8 +8,8 @@ if (NOT UVGRTP_FOUND)
     # uvgRTP
     FetchContent_Declare(
             uvgrtp
-            GIT_REPOSITORY https://github.com/ultravideo/uvgRTP.git
-            GIT_TAG        4762c7e710f9f2e3c56d080b991904ec7a800287
+            GIT_REPOSITORY https://github.com/jrsnen/uvgRTP.git
+            GIT_TAG        2d4d1f7a0a70aa8b72f1d619062299daad8882f5
     )
 
     set(BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
