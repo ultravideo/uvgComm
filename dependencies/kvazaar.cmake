5d86bd4b52150e28ef5675ad6bdddbf7737aa409
Joni Räsänen <joni.rasanen@iki.fi>
2023-06-15 14:55:01 +0300

    build(CMake): Switch to using main Kvazaar repo and add CMake to Kvazzup project
    It looks like the process to get CMake included in Kvazaar will take some time
    so as a better solution than using own repo, this commit adds the CMake file
    to Kvazzup project. This way I can use the main repo and I can also update the
    Kvazaar version easily as long as the new version doesn't break building with
    CMake.
    
    The added Kvazaar CMake is a copy of CMake found in uvg266 created by Marko Viitanen
    with only minor changes.

--- dependencies/kvazaar.cmake
+++ dependencies/kvazaar.cmake
@@ -1,5 +1,6 @@
 include(FetchContent)
 
+
 if (NOT KVAZAAR_FOUND)
     find_package(Git REQUIRED)
 
@@ -8,14 +9,21 @@ if (NOT KVAZAAR_FOUND)
     # Kvazaar
     FetchContent_Declare(
             kvazaar
-            GIT_REPOSITORY https://github.com/jrsnen/kvazaar.git
-            GIT_TAG        0bd045db34c9896f37ce27bc6177c4f3bb1af097
+            GIT_REPOSITORY https://github.com/ultravideo/kvazaar.git
+            GIT_TAG        37a0404bc8ccdc39515a5aed706205dc53810019 # 2.2.0 release
     )
 
     set(BUILD_SHARED_LIBS ON CACHE BOOL "" FORCE)
     set(BUILD_TESTS OFF CACHE BOOL "" FORCE)
 
-    FetchContent_MakeAvailable(kvazaar)
+    if (FALSE) # use this if kvazaar gets a CMake in the main repo
+        FetchContent_MakeAvailable(kvazaar)
+    else()
+        FetchContent_Populate(kvazaar)
+
+        configure_file(dependencies/kvazaar_CMakeLists.txt ${kvazaar_SOURCE_DIR}/CMakeLists.txt COPYONLY)
+        add_subdirectory(${kvazaar_SOURCE_DIR} ${kvazaar_BINARY_DIR} EXCLUDE_FROM_ALL)
+    endif()
 
     unset(BUILD_SHARED_LIBS)
     unset(BUILD_TESTS)
