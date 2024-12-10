b6a109f86473b8d8cb9c8a431ee9d2a33a5f9979
Joni Räsänen <joni.rasanen@iki.fi>
2024-12-10 11:25:39 +0200

    build(Dependencies): Update uvgRTP to version 3.1.4 to fix a critical bug

--- dependencies/uvgrtp.cmake
+++ dependencies/uvgrtp.cmake
@@ -9,7 +9,7 @@ if (NOT UVGRTP_FOUND)
     FetchContent_Declare(
             uvgrtp
             GIT_REPOSITORY https://github.com/ultravideo/uvgRTP.git
-            GIT_TAG        0925db939b62aee050a112530461f29f3a2262d7 # uvgRTP 3.1.3
+            GIT_TAG        2551f270d85ea04ac94b9c1f08814006b7fbc220 # uvgRTP v3.1.4
     )
 
     set(BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
