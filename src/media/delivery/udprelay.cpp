5d79cef7ae6022fa310fd0131297f2f123cb1ca6
Joni Räsänen <joni.rasanen@iki.fi>
2024-12-10 12:02:49 +0200

    fix(Delivery): Better detection of payload types in relay

--- src/media/delivery/udprelay.cpp
+++ src/media/delivery/udprelay.cpp
@@ -50,9 +50,17 @@ void UDPRelay::readPendingDatagrams()
     }
 
     // first figure out if this is and RTP packet or an RTCP packet
-    uint8_t pt = *reinterpret_cast<uint8_t*>(datagram.data() + 1);
+    uint8_t rtcp_pt = *reinterpret_cast<uint8_t*>(datagram.data() + 1);
+    uint8_t rtp_pt = rtcp_pt & 0x7F;
 
-    if (pt < 128)
+    if (rtcp_pt == 200 || rtcp_pt == 201 || rtcp_pt == 202)
+    {
+      // RTCP
+      Logger::getLogger()->printDebug(DEBUG_NORMAL, this, "Received an RTCP packet, not doing anything",
+                                      {"Payload type", "Packet size"},
+                                      {QString::number(rtcp_pt), QString::number(datagram.size())});
+    }
+    else if (rtp_pt <= 34 || 96 <= rtp_pt)
     {
       // RTP
       uint32_t ssrc = *reinterpret_cast<uint32_t*>(datagram.data() + 8);
@@ -76,15 +84,15 @@ void UDPRelay::readPendingDatagrams()
       {
         Logger::getLogger()->printDebug(DEBUG_WARNING, this, "Received an RTP packet for which we have no receiver",
                                         {"SSRC", "Payload type", "Packet size"},
-                                        {QString::number(ssrc), QString::number(pt), QString::number(datagram.size())});
+                                        {QString::number(ssrc), QString::number(rtp_pt), QString::number(datagram.size())});
       }
     }
     else
     {
-      // RTCP
-      Logger::getLogger()->printDebug(DEBUG_WARNING, this, "Received a packet which is not implemented yet",
+      // unknown payload type
+      Logger::getLogger()->printDebug(DEBUG_WARNING, this, "Received a packet which does not follow RTP specifications",
                                       {"Payload type", " Possible SSRC", "Packet size"},
-                                      {QString::number(pt),
+                                      {QString::number(rtp_pt),
                                        QString::number(*reinterpret_cast<uint32_t*>(datagram.data() + 4)),
                                        QString::number(datagram.size())});
 
