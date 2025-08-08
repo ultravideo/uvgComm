679b8aeaeae30c93bd4724e78671cec1844419a4
Joni Räsänen <joni.rasanen@iki.fi>
2025-08-08 15:16:20 +0300

    fix(Docker): Fix executable spelling for uvgComm

--- docker/run.sh
+++ docker/run.sh
@@ -5,5 +5,8 @@ Xvfb :99 -screen 0 1024x768x16 &
 
 export DISPLAY=:99
 
+echo "Running in $(pwd), contents:"
+ls -l
+
 # Launch the application binary — adjust binary name if needed
-./uvgcomm "$@"
+./uvgComm "$@"
